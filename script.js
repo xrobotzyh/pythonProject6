620647391e60c767b022bb014c7c7a98f375cf14
zhao yh <makai87kai@outlook.com>
2023-05-18 17:56:12 +0200

    build the html with api

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 // get all the blocks of images
 const blocks = document.querySelectorAll('.slider-container');
 
@@ -35,3 +34,113 @@ blocks.forEach(block => {
   arrowRight.addEventListener('click',slideRight);
 
 });
+
+function fetchMoviesData(baseUrl, containerId, genre,moviesCount) {
+    let container = document.getElementById(containerId);
+    let slider = container.querySelector(".slider");
+    let moviesLoaded = 0;
+    let page = 1;
+    let moviesPerPage = 5;
+//    while (moviesLoaded < moviesCount) {
+     while (page < 3) {
+        let url = baseUrl + "?genre=" + genre + "&limit=" + moviesPerPage + "&page=" + page + "&sort_by=-imdb_score";
+        fetch(url)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('movies not found'); // throw error message
+                }
+                return response.json();
+            })
+            .then(data => {
+                let moviesToAdd =  Math.min(moviesCount - moviesLoaded, data.results.length);
+                for (let i = 0; i < moviesToAdd; i++) {
+                    let movieElement = createMovieElement(data.results[i]);
+                    slider.appendChild(movieElement);
+                    moviesLoaded++;
+                }
+            })
+            .catch(error => {
+                console.log(error);
+            });
+
+        page++;
+    }
+}
+
+function createMovieElement(movie) {
+    let movieElement = document.createElement("div");
+    movieElement.classList.add("movie");
+
+    let image = new Image();
+    image.src = movie.image_url;
+
+    image.onload = function() {
+        movieElement.innerHTML = `
+            <img src="${movie.image_url}" alt="${movie.title}">
+            <h3>${movie.title}</h3>
+            <button class="view-details-button" data-movie-id="${movie.id}">view details</button>
+        `;
+    };
+
+    image.onerror = function() {
+        console.error('Image not found');
+    };
+
+    return movieElement;
+}
+
+
+function showMovieDetails(movieId) {
+    let modal = document.getElementById("modal");
+    modal.style.display = "block";
+
+    // 使用 Fetch 获取电影详情数据
+    fetch(`http://localhost:8000/api/v1/titles/${movieId}`)
+        .then(response => response.json())
+        .then(data => {
+            let movieDetailsElement = document.querySelector(".movie-details");
+            movieDetailsElement.innerHTML = `
+                <h2>${data.title}</h2>
+                <p>year：${data.year}</p>
+                <p>imdb_score：${data.imdb_score}</p>
+                <p>directors：${data.directors.join(", ")}</p>
+                <p>actors：${data.actors.join(", ")}</p>
+                <p>duration：${data.duration}</p>
+                <p>country：${data.countries.join(", ")}</p>
+                <p>income：${data.worldwide_gross_income}</p>
+                <p>description：${data.description}</p>
+            `;
+        })
+        .catch(error => {
+            console.log(error);
+        });
+}
+
+function closeMovieDetails() {
+    let modal = document.getElementById("modal");
+    modal.style.display = "none";
+}
+
+function initializePage() {
+    fetchMoviesData("http://localhost:8000/api/v1/titles/", "banner","",1);
+    fetchMoviesData("http://localhost:8000/api/v1/titles/", "top-rated-movies-list","",7);
+    fetchMoviesData("http://localhost:8000/api/v1/titles/", "action","Action",7);
+    fetchMoviesData("http://localhost:8000/api/v1/titles/", "animation","Animation",7);
+    fetchMoviesData("http://localhost:8000/api/v1/titles/", "adventure","Adventure",7);
+
+
+    let viewDetailsButtons = document.querySelectorAll(".view-details-button");
+    viewDetailsButtons.forEach(button => {
+        button.addEventListener("click", function() {
+            let movieId = this.getAttribute("data-movie-id");
+            showMovieDetails(movieId);
+        });
+    });
+
+
+    let closeButton = document.querySelector(".close-button");
+    closeButton.addEventListener("click", closeMovieDetails);
+}
+
+
+document.addEventListener("DOMContentLoaded", initializePage);
